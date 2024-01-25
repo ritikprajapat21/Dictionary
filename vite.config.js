c4ca39b76cec5d30c887b76b63d8253b6d96b633
ritiprajapat21 <ritipfb21@svvv.edu.in>
2024-01-25 09:19:12 +0530

    Configuration Changed

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Dictionary/",
   plugins: [react()],
-  server: {
-    port: 3000,
-    host: true,
-  },
 });
